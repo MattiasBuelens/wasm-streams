5b379c00de10ff4bef955b241754f9214e1d448f
Mattias Buelens <mattias@buelens.com>
2020-01-01 17:01:51 +0100

    Abort the pending pull on cancel

--- src/readable/into_underlying_source.rs
+++ src/readable/into_underlying_source.rs
@@ -2,6 +2,7 @@ use std::cell::RefCell;
 use std::pin::Pin;
 use std::rc::Rc;
 
+use futures::future::{abortable, AbortHandle, TryFutureExt};
 use futures::stream::{Stream, TryStreamExt};
 use js_sys::Promise;
 use wasm_bindgen::prelude::*;
@@ -12,24 +13,26 @@ use super::sys;
 #[wasm_bindgen]
 pub(crate) struct IntoUnderlyingSource {
     stream: Rc<RefCell<Option<Pin<Box<dyn Stream<Item = Result<JsValue, JsValue>>>>>>>,
+    pull_handle: Option<AbortHandle>,
 }
 
 impl IntoUnderlyingSource {
     pub fn new(stream: Box<dyn Stream<Item = Result<JsValue, JsValue>>>) -> Self {
         IntoUnderlyingSource {
             stream: Rc::new(RefCell::new(Some(stream.into()))),
+            pull_handle: None,
         }
     }
 }
 
 #[wasm_bindgen]
 impl IntoUnderlyingSource {
-    pub fn pull(&self, controller: sys::ReadableStreamDefaultController) -> Promise {
-        let stream = self.stream.clone();
-        future_to_promise(async move {
+    pub fn pull(&mut self, controller: sys::ReadableStreamDefaultController) -> Promise {
+        let maybe_stream = self.stream.clone();
+        let fut = async move {
             // This mutable borrow can never panic, since the ReadableStream always queues
             // each operation on the underlying source.
-            let mut maybe_stream = stream.borrow_mut();
+            let mut maybe_stream = maybe_stream.borrow_mut();
             // The stream should still exist, since pull() will not be called again
             // after the stream has closed or encountered an error.
             let stream = maybe_stream.as_mut().unwrap_throw();
@@ -47,11 +50,19 @@ impl IntoUnderlyingSource {
                 }
             }
             Ok(JsValue::undefined())
-        })
+        };
+        let (fut, handle) = abortable(fut);
+        self.pull_handle = Some(handle);
+        future_to_promise(fut.unwrap_or_else(|_| Ok(JsValue::from_str("aborted"))))
     }
 
-    pub fn cancel(self) {
-        // The stream has been canceled, drop everything.
+    pub fn cancel(mut self) {
+        // The stream has been canceled.
+        // Abort the pending pull, if any.
+        if let Some(handle) = &mut self.pull_handle {
+            handle.abort();
+        }
+        // Drop everything.
         drop(self);
     }
 }
