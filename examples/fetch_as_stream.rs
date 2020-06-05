e0b66dce2e25e70196ab7823906101fccbb70a47
Mattias Buelens <mattias@buelens.com>
2020-06-05 23:44:57 +0200

    Tweak example

--- examples/fetch_as_stream.rs
+++ examples/fetch_as_stream.rs
@@ -2,7 +2,9 @@
 //!
 //! This example makes an HTTP request using `fetch()` from `web-sys`,
 //! and then consumes the response body as a Rust `Stream`.
+
 use futures::StreamExt;
+use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use wasm_bindgen_futures::JsFuture;
 use web_sys::{console, window, Response};
@@ -13,18 +15,18 @@ use wasm_streams::ReadableStream;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Make a fetch request
     let url = "http://example.com/";
-    let window = window().unwrap();
+    let window = window().unwrap_throw();
     let resp_value = JsFuture::from(window.fetch_with_str(url))
         .await
-        .expect("fetch failed");
-    let resp: Response = resp_value.dyn_into().unwrap();
+        .map_err(|_| "fetch failed")?;
+    let resp: Response = resp_value.dyn_into().unwrap_throw();
 
     // Get the response's body as a ReadableStream
-    let raw_body = resp.body().unwrap();
-    let body = ReadableStream::from_raw(raw_body.unchecked_into());
+    let raw_body = resp.body().unwrap_throw();
+    let body = ReadableStream::from_raw(raw_body.dyn_into().unwrap_throw());
 
     // Convert the ReadableStream to a Rust stream
-    let mut stream = body.into_stream().unwrap();
+    let mut stream = body.into_stream().unwrap_throw();
 
     // Consume the stream
     while let Some(Ok(chunk)) = stream.next().await {
