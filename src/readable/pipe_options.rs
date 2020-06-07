ffb2cf41d2e0081436fe1943b233abbe44aa2876
Mattias Buelens <mattias@buelens.com>
2020-06-07 13:35:35 +0200

    Fix PipeOptions

--- src/readable/pipe_options.rs
+++ src/readable/pipe_options.rs
@@ -1,16 +1,12 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-#[derive(Debug, Default, Clone, PartialEq, Eq)]
+#[derive(Clone, Debug, Default)]
 pub struct PipeOptions {
-    #[wasm_bindgen(readonly, js_name = preventClose)]
-    pub prevent_close: bool,
-    #[wasm_bindgen(readonly, js_name = preventCancel)]
-    pub prevent_cancel: bool,
-    #[wasm_bindgen(readonly, js_name = preventAbort)]
-    pub prevent_abort: bool,
+    prevent_close: bool,
+    prevent_cancel: bool,
+    prevent_abort: bool,
     // TODO abort signal
-    _private: (),
 }
 
 impl PipeOptions {
@@ -18,18 +14,36 @@ impl PipeOptions {
         Default::default()
     }
 
-    pub fn prevent_close(&mut self, prevent_close: bool) -> &mut Self {
+    pub fn set_prevent_close(&mut self, prevent_close: bool) -> &mut Self {
         self.prevent_close = prevent_close;
         self
     }
 
-    pub fn prevent_cancel(&mut self, prevent_cancel: bool) -> &mut Self {
+    pub fn set_prevent_cancel(&mut self, prevent_cancel: bool) -> &mut Self {
         self.prevent_cancel = prevent_cancel;
         self
     }
 
-    pub fn prevent_abort(&mut self, prevent_abort: bool) -> &mut Self {
+    pub fn set_prevent_abort(&mut self, prevent_abort: bool) -> &mut Self {
         self.prevent_abort = prevent_abort;
         self
+    }
+}
+
+#[wasm_bindgen]
+impl PipeOptions {
+    #[wasm_bindgen(getter, js_name = preventClose)]
+    pub fn prevent_close(&self) -> bool {
+        self.prevent_close
+    }
+
+    #[wasm_bindgen(getter, js_name = preventCancel)]
+    pub fn prevent_cancel(&self) -> bool {
+        self.prevent_cancel
+    }
+
+    #[wasm_bindgen(getter, js_name = preventAbort)]
+    pub fn prevent_abort(&self) -> bool {
+        self.prevent_abort
     }
 }
