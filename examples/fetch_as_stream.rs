cff86b6601c52993340ed615aec9dc26a487c169
Mattias Buelens <mattias@buelens.com>
2020-06-15 23:48:46 +0200

    Tweak fetch example
    * Use a real URL
    * Tweak comments
    * Combine use declarations

--- examples/fetch_as_stream.rs
+++ examples/fetch_as_stream.rs
@@ -4,8 +4,7 @@
 //! and then consumes the response body as a Rust `Stream`.
 
 use futures::StreamExt;
-use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
+use wasm_bindgen::{prelude::*, JsCast};
 use wasm_bindgen_futures::JsFuture;
 use web_sys::{console, window, Response};
 
@@ -14,21 +13,21 @@ use wasm_streams::ReadableStream;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Make a fetch request
-    let url = "http://example.com/";
+    let url = "https://rustwasm.github.io/assets/wasm-ferris.png";
     let window = window().unwrap_throw();
     let resp_value = JsFuture::from(window.fetch_with_str(url))
         .await
         .map_err(|_| "fetch failed")?;
     let resp: Response = resp_value.dyn_into().unwrap_throw();
 
-    // Get the response's body as a ReadableStream
+    // Get the response's body as a JS ReadableStream
     let raw_body = resp.body().unwrap_throw();
     let body = ReadableStream::from_raw(raw_body.dyn_into().unwrap_throw());
 
-    // Convert the ReadableStream to a Rust stream
+    // Convert the JS ReadableStream to a Rust stream
     let mut stream = body.into_stream();
 
-    // Consume the stream
+    // Consume the stream, logging each individual chunk
     while let Some(Ok(chunk)) = stream.next().await {
         console::log_1(&chunk);
     }
